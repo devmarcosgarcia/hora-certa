4088e397b9ac94f0e076b1b63512a800aa831033
Marcos Quirino Garcia <marcosqgarcia@gmail.com>
2022-10-04 21:17:24 -0300

    add logicas javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,67 @@
+
+var controleHoras = window.document.querySelector('div#controle input')
+
+controleHoras.addEventListener('input', cliqueControle)
+
+function cliqueControle(){
+
+    var img = window.document.querySelector('div#imagem')
+    
+    var horas = Number(controleHoras.value)
+           
+    
+    if (horas >= 5 && horas <= 7){      //sunrise
+        img.innerHTML = "<img src='imagens/sunrise.png'>"
+    }else if(horas >= 8 && horas <= 11){     //morning
+        img.innerHTML = "<img src='imagens/morning.png'>"
+    }else if(horas >= 12 && horas <= 14){     //noon  
+        img.innerHTML = "<img src='imagens/noon.png'>"
+    }else if(horas >= 15 && horas <= 17){     //afternoon
+        img.innerHTML = "<img src='imagens/afternoon.png'>"
+    }else if(horas >= 18 && horas <= 19){     // evening
+        img.innerHTML = "<img src='imagens/evening.png'>"
+    }else{       //night
+        img.innerHTML = "<img src='imagens/night.png'>"
+    }
+}
+
+
 function carregar(){
     var msgHoras = window.document.querySelector('div#horas')
     var img = window.document.querySelector('div#imagem')
-
+    
     var data = new Date()
     var horas = data.getHours()
     var minutos = data.getMinutes()
-    msgHoras.innerHTML = `${horas}:${minutos}`
 
-        
-    if (horas >= 5 && horas <= 7){
-        img.innerHTML = "<img src='imagens/sunrise.png'>"
+    if (minutos >= 1 && minutos < 10){
+        minutos = `0${minutos}`    
     }
+
+    msgHoras.innerHTML = `${horas}:${minutos}`
+    
+    window.document.querySelector('div#controle input').value = horas
+        
+    if (horas >= 5 && horas <= 7){      //sunrise
+        img.innerHTML = "<img src='imagens/sunrise.png'>"
+    }else if(horas >= 8 && horas <= 11){     //morning
+        img.innerHTML = "<img src='imagens/morning.png'>"
+    }else if(horas == 12 && horas <= 15){     //noon  
+        img.innerHTML = "<img src='imagens/noon.png'>"
+    }else if(horas >= 16 && horas <= 17){     //afternoon
+        img.innerHTML = "<img src='imagens/afternoon.png'>"
+    }else if(horas >= 18 && horas <= 19){     // evening
+        img.innerHTML = "<img src='imagens/evening.png'>"
+    }else{       //night
+        img.innerHTML = "<img src='imagens/night.png'>"
+    }
+
+
+    
 }
+
+function autoRefresh(){
+    window.location = window.location.href
+}
+
+setInterval('autoRefresh()', 5000)
